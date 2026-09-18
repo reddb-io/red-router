AFG473319/9router

fix(translator): keep Responses tool-output images as images
Chat `tool` messages only accept a string, so openaiResponsesToOpenAIRequest()
JSON.stringify()'d an array `function_call_output.output` — the shape Codex uses
for screenshots returned by image tools. Every screenshot then travelled as a few
hundred KB of base64 text, and upstream tokenizers bill that as text (~1.5
chars/token): two screenshots alone added ~500K tokens, pushing a 1M-context
request over its limit with HTTP 400 "maximum context length".

Keep the text parts in the tool message and re-attach the images as a following
user turn, which is how vision-capable chat upstreams expect them. Arrays that
carry no content parts keep the previous JSON.stringify shape.

Also declare the DeepSeek API's Flash models as vision models: deepseek-flash
accepts images (the retired deepseek-v4-flash / -vision-exp ids are served by the
same V4.1-Flash model), yet getCapabilitiesForModel("deepseek",
"deepseek-v4-flash") resolved to vision:false, so a client-supplied image was
replaced with "[image omitted: model has no vision support]". v4-pro stays
text-only, matching DeepSeek's own table.

Tests: tests/unit/responses-tool-output-images.test.js

open-sse/translator/request/openai-responses.js
@@ -16,6 +16,74 @@ import { ROLE, OPENAI_BLOCK, RESPONSES_ITEM } from "../schema/index.js";
 
 const MAX_TOOL_NAME_LEN = 128;
 
+// JSON.stringify that never throws on circular / BigInt payloads.
+function safeJson(value) {
+  try {
+    return JSON.stringify(value) ?? String(value);
+  } catch {
+    return String(value);
+  }
+}
+
+/**
+ * Split a Responses `function_call_output.output` into chat-safe text plus images.
+ *
+ * Responses allows `output` to be an array of content parts, and Codex uses that
+ * to hand back screenshots produced by image tools. A chat `tool` message only
+ * accepts a string, so the old `JSON.stringify(output)` fallback turned every
+ * screenshot into a few hundred KB of base64 *text*. Upstream tokenizers bill
+ * that as text (~1.5 chars/token), so two screenshots alone could add ~500K
+ * tokens and push a 1M-context request past its limit (HTTP 400 "maximum context
+ * length"). Keep the text in the tool message and re-attach the images as a
+ * following user turn, which is how vision-capable chat upstreams expect them.
+ *
+ * @param {unknown} output - function_call_output.output (string | content parts | anything)
+ * @returns {{ text: string, images: Array<{ type: string, image_url: { url: string, detail: string } }> }}
+ */
+function splitToolOutputContent(output) {
+  if (typeof output === "string") return { text: output, images: [] };
+  if (!Array.isArray(output)) return { text: coerceResponsesOutput(output), images: [] };
+
+  const texts = [];
+  const images = [];
+  let contentParts = 0;
+
+  for (const part of output) {
+    if (part === undefined || part === null) continue;
+    if (typeof part === "string") {
+      contentParts++;
+      texts.push(part);
+      continue;
+    }
+    if (part.type === RESPONSES_ITEM.INPUT_TEXT || part.type === OPENAI_BLOCK.TEXT) {
+      contentParts++;
+      if (typeof part.text === "string") texts.push(part.text);
+      continue;
+    }
+    if (part.type === RESPONSES_ITEM.INPUT_IMAGE || part.type === OPENAI_BLOCK.IMAGE_URL) {
+      contentParts++;
+      // Responses sends input_image with a plain string; relay chat-shaped
+      // image_url { url, detail } too, and degrade to text when neither is present.
+      const url = typeof part.image_url === "string"
+        ? part.image_url
+        : part.image_url?.url || part.file_id || "";
+      const detail = typeof part.image_url === "object" ? part.image_url?.detail : part.detail;
+      if (typeof url === "string" && url) {
+        images.push({ type: OPENAI_BLOCK.IMAGE_URL, image_url: { url, detail: detail || "auto" } });
+      } else {
+        texts.push("[image omitted: missing image payload]");
+      }
+      continue;
+    }
+    texts.push(safeJson(part));
+  }
+
+  // Nothing looked like Responses content parts (e.g. output: [1, 2]) — keep the
+  // old JSON.stringify(array) shape so this stays a pure upgrade for known shapes.
+  if (contentParts === 0) return { text: safeJson(output), images: [] };
+  return { text: texts.join("\n"), images };
+}
+
 /**
  * Convert OpenAI Responses API request to OpenAI Chat Completions format
  */
@@ -139,12 +207,20 @@ export function openaiResponsesToOpenAIRequest(model, body, stream, credentials)
         }
         pendingToolResults = [];
       }
-      // Add tool result immediately
+      // Add tool result immediately. Chat `tool` messages only take text, so an
+      // array output keeps its text here and carries its images in a user turn.
+      const { text, images } = splitToolOutputContent(item.output);
       result.messages.push({
         role: ROLE.TOOL,
         tool_call_id: item.call_id,
-        content: typeof item.output === "string" ? item.output : JSON.stringify(item.output)
+        content: text || (images.length > 0 ? "[tool returned images - see the next message]" : "")
       });
+      if (images.length > 0) {
+        result.messages.push({
+          role: ROLE.USER,
+          content: [{ type: OPENAI_BLOCK.TEXT, text: "[images returned by the tool result above]" }, ...images]
+        });
+      }
     }
     else if (itemType === RESPONSES_ITEM.ADDITIONAL_TOOLS) {
       if (Array.isArray(item.tools)) additionalTools.push(...item.tools);

reddb-io/red-router

(cherry picked from commit acfc6618a1fb4b18e931ba369d49ba2f8e8d7bff)

open-sse/translator/request/openai-responses.js
@@ -17,6 +17,74 @@ import { generateToolCallId } from "../concerns/toolCall.js";
 
 const MAX_TOOL_NAME_LEN = 128;
 
+// JSON.stringify that never throws on circular / BigInt payloads.
+function safeJson(value) {
+  try {
+    return JSON.stringify(value) ?? String(value);
+  } catch {
+    return String(value);
+  }
+}
+
+/**
+ * Split a Responses `function_call_output.output` into chat-safe text plus images.
+ *
+ * Responses allows `output` to be an array of content parts, and Codex uses that
+ * to hand back screenshots produced by image tools. A chat `tool` message only
+ * accepts a string, so the old `JSON.stringify(output)` fallback turned every
+ * screenshot into a few hundred KB of base64 *text*. Upstream tokenizers bill
+ * that as text (~1.5 chars/token), so two screenshots alone could add ~500K
+ * tokens and push a 1M-context request past its limit (HTTP 400 "maximum context
+ * length"). Keep the text in the tool message and re-attach the images as a
+ * following user turn, which is how vision-capable chat upstreams expect them.
+ *
+ * @param {unknown} output - function_call_output.output (string | content parts | anything)
+ * @returns {{ text: string, images: Array<{ type: string, image_url: { url: string, detail: string } }> }}
+ */
+function splitToolOutputContent(output) {
+  if (typeof output === "string") return { text: output, images: [] };
+  if (!Array.isArray(output)) return { text: coerceResponsesOutput(output), images: [] };
+
+  const texts = [];
+  const images = [];
+  let contentParts = 0;
+
+  for (const part of output) {
+    if (part === undefined || part === null) continue;
+    if (typeof part === "string") {
+      contentParts++;
+      texts.push(part);
+      continue;
+    }
+    if (part.type === RESPONSES_ITEM.INPUT_TEXT || part.type === OPENAI_BLOCK.TEXT) {
+      contentParts++;
+      if (typeof part.text === "string") texts.push(part.text);
+      continue;
+    }
+    if (part.type === RESPONSES_ITEM.INPUT_IMAGE || part.type === OPENAI_BLOCK.IMAGE_URL) {
+      contentParts++;
+      // Responses sends input_image with a plain string; relay chat-shaped
+      // image_url { url, detail } too, and degrade to text when neither is present.
+      const url = typeof part.image_url === "string"
+        ? part.image_url
+        : part.image_url?.url || part.file_id || "";
+      const detail = typeof part.image_url === "object" ? part.image_url?.detail : part.detail;
+      if (typeof url === "string" && url) {
+        images.push({ type: OPENAI_BLOCK.IMAGE_URL, image_url: { url, detail: detail || "auto" } });
+      } else {
+        texts.push("[image omitted: missing image payload]");
+      }
+      continue;
+    }
+    texts.push(safeJson(part));
+  }
+
+  // Nothing looked like Responses content parts (e.g. output: [1, 2]) — keep the
+  // old JSON.stringify(array) shape so this stays a pure upgrade for known shapes.
+  if (contentParts === 0) return { text: safeJson(output), images: [] };
+  return { text: texts.join("\n"), images };
+}
+
 /**
  * Convert OpenAI Responses API request to OpenAI Chat Completions format
  */
@@ -167,7 +235,9 @@ export function openaiResponsesToOpenAIRequest(model, body, stream, credentials)
       // `role:"tool"` message without `tool_call_id`, which every strict upstream
       // rejects (NVIDIA 400 "missing field `tool_call_id`", OpenAI "must be a
       // response to a message with tool_calls").
-      const outputContent = typeof item.output === "string" ? item.output : JSON.stringify(item.output);
+      // Chat `tool` messages only take text, so an array output keeps its text
+      // here and carries its images in a user turn (#4058).
+      const { text: outputContent, images: outputImages } = splitToolOutputContent(item.output);
       let outputCallId = typeof item.call_id === "string" && item.call_id ? item.call_id : "";
       if (outputCallId) {
         const queued = pendingToolCallIds.indexOf(outputCallId);
@@ -179,8 +249,14 @@ export function openaiResponsesToOpenAIRequest(model, body, stream, credentials)
         result.messages.push({
           role: ROLE.TOOL,
           tool_call_id: outputCallId,
-          content: outputContent
+          content: outputContent || (outputImages.length > 0 ? "[tool returned images - see the next message]" : "")
         });
+        if (outputImages.length > 0) {
+          result.messages.push({
+            role: ROLE.USER,
+            content: [{ type: OPENAI_BLOCK.TEXT, text: "[images returned by the tool result above]" }, ...outputImages]
+          });
+        }
       }
       // else: orphaned output (no pending call to answer) is dropped, matching the
       // orphan-repair contract tracked in #2236 — a tool message without a call is